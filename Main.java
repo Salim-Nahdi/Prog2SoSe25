ba60e791d9377b3963ca77dbeac9af63761c9126
Salim <salim.nahdi@stud.uni-hannover.de>
2025-07-08 23:12:43 +0200

    Added Icon

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 import View.MenuScene;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
+        Image icon = new Image(getClass().getResourceAsStream("/assets/banner.png"));
+        primaryStage.getIcons().add(icon);
         MenuScene menuScene = new MenuScene(primaryStage);
         primaryStage.setScene(menuScene.getScene());
         primaryStage.setTitle("Main Menu");
